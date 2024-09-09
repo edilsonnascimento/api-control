b0f52839ac21a9e2bb2df763d6edbd757806dd21
Edilson do Nascimento <edilson.nascimento@nexti.com>
2024-09-09 10:06:18 -0300

    1 - ADD new project.

---
+++ src/main/java/com/nexti/api/control/ApiControlApplication.java
@@ -0,0 +1,13 @@
+package com.nexti.api.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControlApplication.class, args);
+	}
+
+}
